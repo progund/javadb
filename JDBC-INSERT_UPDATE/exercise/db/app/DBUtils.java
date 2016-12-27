c073146a4abbe62d5f7ed14351a647d73e0134db
Rikard Fröberg <rikard@morus.se>
2016-12-27 18:50:08 +0100

    fixed indentation

--- JDBC-INSERT_UPDATE/exercise/db/app/DBUtils.java
+++ JDBC-INSERT_UPDATE/exercise/db/app/DBUtils.java
@@ -12,105 +12,105 @@ import java.sql.*;
  */
 
 public class DBUtils{
-    private final static String DB_CONN_STR="jdbc:sqlite:my_municipalities";
-    static{
-        try{
-            Class.forName("org.sqlite.JDBC");
-        }catch(ClassNotFoundException cnfe){
-            System.err.println("Could not load driver: "+cnfe.getMessage());
-        }
+  private final static String DB_CONN_STR="jdbc:sqlite:my_municipalities";
+  static{
+    try{
+      Class.forName("org.sqlite.JDBC");
+    }catch(ClassNotFoundException cnfe){
+      System.err.println("Could not load driver: "+cnfe.getMessage());
     }
-    private static Connection con;
-    private static DBUtils instance = new DBUtils();
-    private DBUtils(){
-	getConnection();
-    }
-    /**
-     * Singleton method to get the instance of this class. 
-     * @return The only instance of this class
-     */
-    public static DBUtils getInstance(){
-	return instance;
-    }
+  }
+  private static Connection con;
+  private static DBUtils instance = new DBUtils();
+  private DBUtils(){
+    getConnection();
+  }
+  /**
+   * Singleton method to get the instance of this class. 
+   * @return The only instance of this class
+   */
+  public static DBUtils getInstance(){
+    return instance;
+  }
     
-    private void getConnection(){
-        try{
-            con = DriverManager.getConnection(DB_CONN_STR);
-        }catch(Exception e){
-            System.err.println("Error getting connection to " + 
-                               DB_CONN_STR);
-        }
+  private void getConnection(){
+    try{
+      con = DriverManager.getConnection(DB_CONN_STR);
+    }catch(Exception e){
+      System.err.println("Error getting connection to " + 
+                         DB_CONN_STR);
     }
-    /**
-     * Checks that the instance has a Connection to the database.
-     * @return true if the instance has a connection, false otherwise
-     */
-    public boolean hasConnection(){
-        return con != null;
-    }
+  }
+  /**
+   * Checks that the instance has a Connection to the database.
+   * @return true if the instance has a connection, false otherwise
+   */
+  public boolean hasConnection(){
+    return con != null;
+  }
 
-    private void error(String msg){ System.err.println(msg); }
+  private void error(String msg){ System.err.println(msg); }
 
 
-    /**
-     * Executes a query (from the argument sql, which is a String) and returns a ResultSet.
-     * @return A ResultSet from the query
-     * @param sql The String representing the SQL query to be executed
-     */
-    public ResultSet executeQuery(String sql){
-	Statement stm=null;
-	if(hasConnection()){
-	    try{
-		stm=con.createStatement();
-		return stm.executeQuery(sql);
-	    }catch(Exception e){
-		error("executeQuery: " + e.getMessage());
-		closeIt(stm);
-	    }
-	}
-	return null;
+  /**
+   * Executes a query (from the argument sql, which is a String) and returns a ResultSet.
+   * @return A ResultSet from the query
+   * @param sql The String representing the SQL query to be executed
+   */
+  public ResultSet executeQuery(String sql){
+    Statement stm=null;
+    if(hasConnection()){
+      try{
+        stm=con.createStatement();
+        return stm.executeQuery(sql);
+      }catch(Exception e){
+        error("executeQuery: " + e.getMessage());
+        closeIt(stm);
+      }
     }
-    /**
-     * Executes an SQL update statement, provided as the String argument
-     * @param sql The SQL update statement as a String
-     * @return An int representing how many rows were updated or -1 if the statement fails
-     *
-     */
-    public int executeUpdate(String sql){
-	Statement stm=null;
-	if(hasConnection()){
-	    try{
-		stm=con.createStatement();
-		return stm.executeUpdate(sql);
-	    }catch(Exception e){
-		error("executeUpdate: "+e.getMessage());
-	    }finally{
-		closeIt(stm);
-	    }
-	}
-	return -1;
+    return null;
+  }
+  /**
+   * Executes an SQL update statement, provided as the String argument
+   * @param sql The SQL update statement as a String
+   * @return An int representing how many rows were updated or -1 if the statement fails
+   *
+   */
+  public int executeUpdate(String sql){
+    Statement stm=null;
+    if(hasConnection()){
+      try{
+        stm=con.createStatement();
+        return stm.executeUpdate(sql);
+      }catch(Exception e){
+        error("executeUpdate: "+e.getMessage());
+      }finally{
+        closeIt(stm);
+      }
     }
-    /**
-     * Closes an AutoCloseable, for instance a ResultSet or a Statement.
-     * @param it The AutoCloseable to be closed
-     */
-    public void closeIt(AutoCloseable it){     
-        try{
-            it.close();
-        }catch(Exception e){
-            error("Exception closing: "+e.getMessage());
-        }
+    return -1;
+  }
+  /**
+   * Closes an AutoCloseable, for instance a ResultSet or a Statement.
+   * @param it The AutoCloseable to be closed
+   */
+  public void closeIt(AutoCloseable it){     
+    try{
+      it.close();
+    }catch(Exception e){
+      error("Exception closing: "+e.getMessage());
     }
-    /* 
-    //What is this? Try to figure out!
-    public PreparedStatement preparedStatement(String sql){
-	try{
-	    return con.prepareStatement(sql);
-	}catch(Exception e){
-	    System.err.println("DBUtils: Error getting prepared stm: " + e.getMessage());
-	    e.printStackTrace();
-	    return null;
-	}
-    }
-    */
+  }
+  /* 
+  //What is this? Try to figure out!
+  public PreparedStatement preparedStatement(String sql){
+  try{
+  return con.prepareStatement(sql);
+  }catch(Exception e){
+  System.err.println("DBUtils: Error getting prepared stm: " + e.getMessage());
+  e.printStackTrace();
+  return null;
+  }
+  }
+  */
 }
--- JDBC-INSERT_UPDATE/exercise/db/app/MunicipalityDB.java
+++ JDBC-INSERT_UPDATE/exercise/db/app/MunicipalityDB.java
@@ -7,38 +7,38 @@ import java.util.List;
  * a method for retrieving one db.app.Municipality by its name.
  */
 public interface MunicipalityDB{
-    /**
-     * Returns a java.util.List of all municipalities as db.app.Municipality objects.
-     * @return a java.util.List of all municipalities as db.app.Municipality objects.
-     *
-     */
-    public List<Municipality>getAllCities();
-    /**
-     * Updates a municipality using its ID as condition for the DB query and updates all fields according to their current status.
-     * @param m The Municipality to be updated
-     */
-    public void updateCity(Municipality m);
-    /**
-     * Deletes a Municipality according using its ID as condition for the query to the database.
-     * @param m The Municipality to be updated
-     */
-    public void deleteCity(Municipality m);
+  /**
+   * Returns a java.util.List of all municipalities as db.app.Municipality objects.
+   * @return a java.util.List of all municipalities as db.app.Municipality objects.
+   *
+   */
+  public List<Municipality>getAllCities();
+  /**
+   * Updates a municipality using its ID as condition for the DB query and updates all fields according to their current status.
+   * @param m The Municipality to be updated
+   */
+  public void updateCity(Municipality m);
+  /**
+   * Deletes a Municipality according using its ID as condition for the query to the database.
+   * @param m The Municipality to be updated
+   */
+  public void deleteCity(Municipality m);
 
-    /**
-     * Adds a Municipality to the database and sets its ID to the newly created records ID
-     * @param m The Municipality to be updated
-     */
-    public void addCity(Municipality m);
+  /**
+   * Adds a Municipality to the database and sets its ID to the newly created records ID
+   * @param m The Municipality to be updated
+   */
+  public void addCity(Municipality m);
     
-    /**
-     * Returns a reference to a new Municipality, which is fetched by name from the database.
-     * @param name A String with the name of the municipality to fetch.
-     */
-    public Municipality getByName(String name);
+  /**
+   * Returns a reference to a new Municipality, which is fetched by name from the database.
+   * @param name A String with the name of the municipality to fetch.
+   */
+  public Municipality getByName(String name);
     
-    /**
-     * Updates a municipality's https status in the database, using its name for condition in the query
-     */
-    public int updateHTTPSbyName(String name, boolean https);
-    //public int updateHTTPSbyName2(String name, boolean https);
+  /**
+   * Updates a municipality's https status in the database, using its name for condition in the query
+   */
+  public int updateHTTPSbyName(String name, boolean https);
+  //public int updateHTTPSbyName2(String name, boolean https);
 }
