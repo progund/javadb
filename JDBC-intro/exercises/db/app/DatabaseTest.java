973abd4b86ba5cd866bc2bf84f5723881c97d27d
Rikard Fröberg <rikard@morus.se>
2018-02-03 22:14:56 +0100

    Coding style changes

--- JDBC-intro/exercises/db/app/DatabaseTest.java
+++ JDBC-intro/exercises/db/app/DatabaseTest.java
@@ -1,68 +1,86 @@
 package db.app;
+
 import java.sql.*;
 
-public class DatabaseTest{
+public class DatabaseTest {
   
   private final static String DB_CONN_STR="jdbc:sqlite:my_municipalities";
-  static{
-    try{
+  private static Connection con;
+
+  static {
+    try {
       Class.forName("org.sqlite.JDBC");
-    }catch(ClassNotFoundException cnfe){
+    } catch (ClassNotFoundException cnfe) {
       System.err.println("Could not load driver: "+cnfe.getMessage());
     }
   }
   
-  private static Connection con;
-  public DatabaseTest(){
+  public DatabaseTest() {
     getConnection();
   }
-  private void getConnection(){
-    try{
+  
+  private void getConnection() {
+    
+    try {
       con = DriverManager.getConnection(DB_CONN_STR);
-    }catch(Exception e){
+    } catch (Exception e) {
       System.err.println("Error getting connection to " + 
                          DB_CONN_STR);
     }
   }
-  public boolean hasConnection(){
+  
+  public boolean hasConnection() {
     return con != null;
   }
-  public void testQuery(){
-    if(hasConnection()){
+  
+  public void testQuery() {
+    
+    if (hasConnection()) {
+
       Statement stm = null;
       ResultSet rs  = null;
-      try{
-        String query="SELECT Name, HTTPS FROM municipalities LIMIT 5";
+
+      try {
+        String query = "SELECT Name, HTTPS FROM municipalities LIMIT 5";
         stm = con.createStatement();
         rs = stm.executeQuery(query);
-        while(rs.next()){
+        
+        while (rs.next()) {
           System.out.println(rs.getString("Name") +
                              " " +
-                             ( rs.getBoolean("HTTPS")?
-                               "HTTPS support":
+                             ( rs.getBoolean("HTTPS") ?
+                               "HTTPS support" :
                                "HTTP only" )
                              );
         }
-      }catch(SQLException sqle){
+        
+      } catch (SQLException sqle) {
         System.err.println(sqle.getMessage());
-      }finally{
+      } finally {
         closeBoth(rs, stm);
       }
     }
   }
-  private void closeIt(AutoCloseable it){     
-    try{
+  
+  private void closeIt(AutoCloseable it) {
+    
+    try {
       it.close();
-    }catch(Exception e){
-      System.err.println("Exception closing: "+e.getMessage());
+    } catch (Exception e) {
+      System.err.println("Exception closing: " + e.getMessage());
     }
+    
   }
-  private void closeBoth(AutoCloseable a1, AutoCloseable a2){
-    try{
+  
+  private void closeBoth(AutoCloseable a1, AutoCloseable a2) {
+
+    try {
       closeIt(a1);          
       closeIt(a2);
-    }catch(Exception e){
-      System.err.println("Exception closing: "+e.getMessage());
+    } catch (Exception e) {
+      System.err.println("Exception closing: " + e.getMessage());
     }
-  }    
+    
+  }
+  
 }
