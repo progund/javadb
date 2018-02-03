a124fc4a32e3e91971901dac511f5ea83f73dc4e
Rikard Fröberg <rikard@morus.se>
2018-02-03 21:12:53 +0100

    Coding style changes

--- assignment-sql-injection/db/main/Main.java
+++ assignment-sql-injection/db/main/Main.java
@@ -1,10 +1,12 @@
 package db.main;
+
 import db.app.*;
 
-public class Main{
-  public static void main(String[] args){
+public class Main {
+  
+  public static void main(String[] args) {
     MunicipalityDB db = new MyMunicipalities();
     /* Add statements below here as per the instructions for handin 03 */
-	
+    
   }
 }
