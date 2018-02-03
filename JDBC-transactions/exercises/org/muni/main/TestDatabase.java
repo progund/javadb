70c4232f35cd8fafa5934a339304e8ca60a7aa92
Rikard Fröberg <rikard@morus.se>
2018-02-03 22:21:46 +0100

    Coding style changes

--- JDBC-transactions/exercises/org/muni/main/TestDatabase.java
+++ JDBC-transactions/exercises/org/muni/main/TestDatabase.java
@@ -1,47 +1,62 @@
 package org.muni.main;
+
 import java.sql.*;
+
 import org.muni.util.DatabaseUtil;
 
-public class TestDatabase{
-  public static void main(String[] args){
+public class TestDatabase {
+  
+  public static void main(String[] args) {
+    
     DatabaseUtil database = new DatabaseUtil();
-    if(! database.hasConnection()){
+
+    if (! database.hasConnection()) {
       System.err.println("No connection.");
       System.exit(1);
     }
+    
     PreparedStatement ps = null;
     ResultSet rs = null;
-    try{
+
+    try {
+      
       database.setAutoCommit(false);    
       String sql = "UPDATE municipalities SET Server=? WHERE Server LIKE ?";
       ps = database.getPS(sql);
       ps.setString(1, "Mickey Mouse");
       ps.setString(2, "microsoft%");
       ps.executeUpdate();
-      try{
+
+      try {
         ps.close();
-      }catch(SQLException sqle){}
+      } catch (SQLException sqle) {}
+      
       sql = "SELECT COUNT(*) FROM municipalities WHERE Server LIKE ?";
       ps = database.getPS(sql);
       ps.setString(1, "microsoft%");
       ps.execute();
       rs = ps.getResultSet();
-      if(rs.next()){
+
+      if (rs.next()) {
         System.out.println("Number of servers like microsoft: " + rs.getInt(1));
       }
+      
       database.rollback();
       ps.execute();
       rs = ps.getResultSet();
-      if(rs.next()){
+
+      if (rs.next()) {
         System.out.println("Number of servers like microsoft: " + rs.getInt(1));
-      }      
-    }catch(SQLException e){
+      }
+      
+    } catch (SQLException e) {
       System.err.println("Database exception: " + e.getMessage());
-    }finally{
-      try{
+    } finally {
+      try {
         ps.close();
         rs.close();
-      }catch(SQLException sqle){}
+      } catch (SQLException sqle) {}
     }
+    
   }
 }
