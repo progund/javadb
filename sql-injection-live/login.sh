0c8b259be6cae9067b4020c30ac9fa462a13031d
Rikard Fröberg <rikard@morus.se>
2019-03-02 16:05:32 +0100

    Script vulnerable to SQL injection

---
+++ sql-injection-live/login.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERNAME="$1"
+PASSWORD="$2"
+
+SQL="SELECT real_name FROM user NATURAL JOIN login WHERE username='$USERNAME' AND password = '$PASSWORD';"
+
+echo $SQL
+exit 0
+
+REAL_NAME=$(sqlite3 login.db "$SQL")
+
+WELCOME_MSG="Wrong username or password"
+if [[ ! -z "$REAL_NAME" ]]
+then
+    WELCOME_MSG="Welcome $REAL_NAME"
+fi
+
+echo "$WELCOME_MSG"
